21a5fdc55675a48dcb8ebbc92d4d08abc7cb5ca7
Annika Neumann <annika1983neumann@gmail.com>
2022-02-22 16:54:12 +0000

    First commit

---
+++ exercise_a_stop.py
@@ -0,0 +1,14 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket"]
+
+# stops.append("Waverly")
+# print(stops)
+
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket"]
+# stops.insert(0, "Glasgow Queens Street")
+# stops.insert(2, "Polmont")
+
+#print(stops[3])
+
+#stops.remove("Livingston")
+#print(stops)
+
